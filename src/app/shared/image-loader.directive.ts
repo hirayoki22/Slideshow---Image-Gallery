0bd1f9a6704a14eb9ab3f6305aba890fce920f95
Adison Pena <hirayoki.nova@gmail.com>
2020-05-28 16:33:04 -0400

    Applied image-loader directive

--- src/app/shared/image-loader.directive.ts
+++ src/app/shared/image-loader.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appImageLoader]'
+  selector: '[image-loader]'
 })
 export class ImageLoaderDirective {
 
